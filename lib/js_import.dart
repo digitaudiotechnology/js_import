5825fbee2ff38b04a522c24d45cf73a75ae4b5b0
Cyril Leung <cyrilhl@gmail.com>
2024-11-06 01:18:45 +0000

    - update dependency

--- lib/js_import.dart
+++ lib/js_import.dart
@@ -1,7 +1,7 @@
 //  Importing JS sources into Flutter Web projects.
 //  Check README: https://github.com/ivangalkindeveloper/js_import/blob/master/README.md
 
-library js_import;
+//library js_import;
 
 export 'src/js_import_stub.dart'
     if (dart.library.js_interop) 'src/js_import_html.dart'; // dart:io implementation
